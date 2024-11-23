faa3e8ce84c72b76959c6a8e8c6581c69e1a356d
Sachin3889 <sachin3889.skiit@gmail.com>
2024-11-23 18:47:26 +0530

    First Commit

---
+++ src/continueDailyPractice/Hello.java
@@ -0,0 +1,10 @@
+package continueDailyPractice;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Sachin");
+	}
+
+}
